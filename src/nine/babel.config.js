12aab6814a338e43b197c7c04c76fa56eb9ef7c5
jinweiOS <jinwei.peng@outlook.com>
2022-07-19 14:04:10 +0800

    feat: babel preset

---
+++ src/nine/babel.config.js
@@ -0,0 +1,7 @@
+// const presets = ["@babel/preset-env"]
+const presets = ["@vue/babel-preset-app"]
+
+module.exports = {
+    presets: presets
+}
+
---
+++ src/nine/src/index.js
@@ -0,0 +1,28 @@
+// // 获取自己电脑上当前年月日
+// const date = new Date()
+// const yearMonDay = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`
+
+// function getMyComputerDay() {
+
+//     return yearMonDay
+// }
+
+
+// // nodejs 模块导入导出
+// module.exports = {
+//     getDay: getMyComputerDay,
+//     a: 1
+// }
+const a = () => console.log(1)
+a()
+// class C {
+//     @enumerable(false)
+//     method() { }
+// }
+
+// function enumerable(value) {
+//     return function (target, key, descriptor) {
+//         descriptor.enumerable = value;
+//         return descriptor;
+//     };
+// }
